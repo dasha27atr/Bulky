1771cdc54e8926b128958ed2014bf8dd7ed032cc
Darya.Atrashevskaya <Darya.Atrashevskaya@b1.by>
2023-12-12 11:38:58 +0300

    IRepository Interface added.

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,9 +1,14 @@
-﻿namespace Bulky.DataAccess.Repository.IRepository
+﻿using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.IRepository
 {
     internal interface IRepository<T> where T : class
     {
         // T - Category
         IEnumerable<T> GetAll();
-        T Get();
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
