8ff8fb4e1d2e2b1feefa85d3dd446879021c8c50
Darya.Atrashevskaya <Darya.Atrashevskaya@b1.by>
2023-12-13 16:46:43 +0300

    product repository and configuration for unit of work added.

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+        }
+    }
+}
